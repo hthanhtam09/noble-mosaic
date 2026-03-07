6bdb55818cad084c5b23823e53e36818ed808cec
Tam Ho Thanh <tamht@kozo-japan.com>
2026-03-08 00:46:40 +0700

    update code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -10,28 +15,28 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: '*.r2.cloudflarestorage.com',
+        protocol: "https",
+        hostname: "*.r2.cloudflarestorage.com",
       },
       {
-        protocol: 'https',
-        hostname: '*.r2.dev',
+        protocol: "https",
+        hostname: "*.r2.dev",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: '*.cloudinary.com',
+        protocol: "https",
+        hostname: "*.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
     ],
   },
